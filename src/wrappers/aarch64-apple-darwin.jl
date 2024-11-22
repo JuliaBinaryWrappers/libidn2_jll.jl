4c23dbb31da8982644b340313662b06db3475457
jlbuild <juliabuildbot@gmail.com>
2024-11-22 22:32:46 +0000

    libidn2_jll build 2.3.7+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -4,19 +4,19 @@ export idn2, libidn2
 using Libiconv_jll
 using libunistring_jll
 JLLWrappers.@generate_wrapper_header("libidn2")
-JLLWrappers.@declare_executable_product(idn2)
 JLLWrappers.@declare_library_product(libidn2, "@rpath/libidn2.0.dylib")
+JLLWrappers.@declare_executable_product(idn2)
 function __init__()
     JLLWrappers.@generate_init_header(Libiconv_jll, libunistring_jll)
-    JLLWrappers.@init_executable_product(
-        idn2,
-        "bin/idn2",
-    )
-
     JLLWrappers.@init_library_product(
         libidn2,
         "lib/libidn2.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        idn2,
+        "bin/idn2",
     )
 
     JLLWrappers.@generate_init_footer()
