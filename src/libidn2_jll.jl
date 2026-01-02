480f2a6fe95a487112bbae462335f76bf34a7665
jlbuild <juliabuildbot@gmail.com>
2026-01-02 01:49:18 +0000

    libidn2_jll build 2.3.8+0

--- src/libidn2_jll.jl
+++ src/libidn2_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libidn2")
-JLLWrappers.@generate_main_file("libidn2", UUID("e3d30ef0-22f3-5ccc-b703-2e063d8d1f30"))
+JLLWrappers.@generate_main_file("libidn2", Base.UUID("e3d30ef0-22f3-5ccc-b703-2e063d8d1f30"))
 end  # module libidn2_jll
